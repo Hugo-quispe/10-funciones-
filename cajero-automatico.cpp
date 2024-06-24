2bb04e6da0a2742494dc423abe32d8ac3f33725f
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-06-24 10:26:34 -0500

    implementacion de una estructura switch y tambien una variable global para el almacenamiento

--- cajero-automatico.cpp
+++ cajero-automatico.cpp
@@ -1,11 +1,29 @@
 #include<iostream>
 using namespace std;
+float saldo;
 int main(){
-    int a;
+    int a,b;
     cout<<"cajero automatico"<<endl;
     cout<<"1 depositar"<<endl;
     cout<<"2 retirar"<<endl;
     cout<<"3 ver saldo"<<endl;
     cout<<"0 salir"<<endl;
     cout<<"ingresa la opcion que prefiera: ";cin>>a;
+    switch(a){
+        case 1:
+            cout<<"ingresa el monto a depositar: ";cin>>b;
+        break;
+        case 2:
+        cout<<"ingresa el monto a retirar: ";cin>>b;
+        break;
+        case 3:
+        cout<<"el saldo actual es: "<<saldo<<endl;
+        break;
+        case 0:
+        cout<<"gracias por usar el programa."<<endl;
+        break;
+        default:
+        cout<<"ingrese una opcion valida."<<endl;
+        break;
+    }
 }
