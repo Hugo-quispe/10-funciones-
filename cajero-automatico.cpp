341e1d290b5dbb4362a77d7a05186df1c9ec2b99
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-06-24 10:16:25 -0500

    se realiza el listado del menu

---
+++ cajero-automatico.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"cajero automatico"<<endl;
+    cout<<"1 depositar"<<endl;
+    cout<<"2 retirar"<<endl;
+    cout<<"3 ver saldo"<<endl;
+    cout<<"0 salir"<<endl;
+    cout<<"ingresa la opcion que prefiera: ";cin>>a;
+}
